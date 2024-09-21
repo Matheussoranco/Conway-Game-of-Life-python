2e735692208993ad418fac71b0edd28f764af834
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-09-21 16:17:00 -0300

    Created main.py and initial grid

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+import os
+import random
+import sys
+
+def create_initial_grid(rows, cols):
+
+    grid = []
+    for row in range(rows):
+        grid_rows = []
+        for col in range(cols):
+            if random.randint(0, 7) == 0:
+                grid_rows += [1]
+            else:
+                grid_rows += [0]
+        grid += [grid_rows]
+    return grid
+
