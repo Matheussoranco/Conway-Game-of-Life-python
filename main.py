aff86fbe5b4af2108a3ca987759ba2c7c02f1935
Matheus Pullig Soranço de Carvalho <matheussoranco@gmail.com>
2024-10-02 13:29:53 -0300

    Printing and cleaning grid

--- main.py
+++ main.py
@@ -16,3 +16,18 @@ def create_initial_grid(rows, cols):
         grid += [grid_rows]
     return grid
 
+def print_grid(rows, cols, grid, generation):
+    clear_console()
+
+    output_str = ""
+
+    output_str += "Generation {0} - To exit the program press <Ctrl-C>\n\r".format(generation)
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 0:
+                output_str += ". "
+            else:
+                output_str += "@ "
+        output_str += "\n\r"
+    print(output_str, end=" ")
+
